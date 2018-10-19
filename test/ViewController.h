90664268efea3aa83790eea6a690edf8fd32326f
顾金 <gujin@gujindemac-mini.local>
2018-10-19 17:26:54 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 顾金 on 2018/10/19.
+//  Copyright © 2018年 徐磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 顾金 on 2018/10/19.
+//  Copyright © 2018年 徐磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
